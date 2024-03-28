54e7e316c90b6793daa4b9be0907ceae7df8a31e
Declan L. Scott <declanlscott@gmail.com>
2024-03-28 18:18:31 -0500

    refactor: generate route tree with vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -7,4 +8,7 @@ export default defineConfig({
   output: "hybrid",
   integrations: [react()],
   adapter: cloudflare(),
+  vite: {
+    plugins: [TanStackRouterVite()],
+  },
 });
